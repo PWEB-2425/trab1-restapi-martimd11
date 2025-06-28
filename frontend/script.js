6af750c3327c47fab4f2578830bbc6d74588698f
Martim <martimd@ipvc.pt>
2025-06-28 17:29:50 +0100

    atualizado

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const baseURL = "https://seu-backend.onrender.com/api/alunos"; // ATUALIZE COM SUA URL
+const baseURL = "https://trab1-restapi-martimd11-t4qs.onrender.com"; // ATUALIZE COM SUA URL
 let alunoIdEditando = null;
 
 // Função aprimorada para carregar alunos
