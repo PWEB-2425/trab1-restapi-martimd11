a7e8955515cc666cff983add018cf9a2f62dd18d
Martim <martimd@ipvc.pt>
2025-06-28 17:07:15 +0100

    atualizado

--- backend/routes/alunoRoutes.js
+++ backend/routes/alunoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Aluno = require("../models/aluno");
 
-// Mudar todas as rotas para /alunos (plural)
+// Altere todas para /alunos (plural)
 router.get("/alunos", async (req, res) => res.json(await Aluno.find()));
 router.post("/alunos", async (req, res) => res.status(201).json(await new Aluno(req.body).save()));
 router.put("/alunos/:id", async (req, res) => res.json(await Aluno.findByIdAndUpdate(req.params.id, req.body, { new: true })));
